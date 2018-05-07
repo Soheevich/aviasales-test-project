d9e34e437f4499e4d0fd39ee255accad615b31bc
Andrei F <soheevich@gmail.com>
2018-05-07 09:26:05 +0300

    Move to typescript

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+
+import App from './App';
+import './index.css';
+import registerServiceWorker from './registerServiceWorker';
+
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
 
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const rootReducer = combineReducers({
+const rootReducer = combineReducers({});
 
-});
-
-const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(thunk))
+);
 
 const app = (
   <Provider store={store}>
@@ -25,5 +28,8 @@ const app = (
   </Provider>
 );
 
-ReactDOM.render(app, document.getElementById('root'));
+ReactDOM.render(
+  app,
+  document.getElementById('root') as HTMLElement
+);
 registerServiceWorker();
