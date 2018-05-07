d37a911dae2ab2793525fd585ba8b724e04beed4
Andrei F <soheevich@gmail.com>
2018-05-07 12:01:25 +0300

    Erase and start from scratch

--- src/index.tsx
+++ src/index.tsx
@@ -1,35 +1,11 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-
-
 import App from './App';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
-import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
-import thunk from 'redux-thunk';
-
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-const rootReducer = combineReducers({});
-
-const store = createStore(
-  rootReducer,
-  composeEnhancers(applyMiddleware(thunk))
-);
-
-const app = (
-  <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>
-);
-
 ReactDOM.render(
-  app,
+  <App />,
   document.getElementById('root') as HTMLElement
 );
 registerServiceWorker();
